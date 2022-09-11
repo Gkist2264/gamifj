5cefd251877d47cae7b5dfefa33e479ef7d45eac
Gkist2264 <111760301+Gkist2264@users.noreply.github.com>
2022-09-11 17:54:11 +0530

    Add files via upload

---
+++ clm/cfg.php
@@ -0,0 +1,10 @@
+<?php
+
+//FILL IN USER-AGENT
+$user = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36 Edg/105.0.1343.33';
+
+//FILL IN COOKIE
+$cookie = 'PHPSESSID=guqd91uvqnf09krv2f7h34o17p; PT_REF_ID=181492; _gid=GA1.2.758334530.1662896937; bitmedia_fid=eyJmaWQiOiI5NjNmMmUxNjE1MmIwNzAxODk3NzA1NzU4ODA4MGJhMyIsImZpZG5vdWEiOiIwYTU5MzUyYzFmM2RkN2JkOWQ0MjljMDc0ZGU1YWZmMiJ9; AccExist=220655; SesHashKey=6g55tj22uy7bftzh; SesToken=ses_id=220655&ses_key=6g55tj22uy7bftzh; _ga_J2L7YD89W2=GS1.1.1662896936.1.1.1662897460.0.0.0; _ga=GA1.2.1915773000.1662896937';
+
+//FILL IN URL-SOLVEMEDIA
+$url_solvemedia = 'https://api-secure.solvemedia.com/papi/_challenge.js?k=rrwQ-WMpY45BeSxuvBbp7jSE-mtSUzY1;f=_ACPuzzleUtil.callbacks%5B0%5D;l=en;t=img;s=standard;c=js,h5c,h5ct,svg,h5v,v/h264,v/webm,h5a,a/mp3,a/ogg,ua/chrome,ua/chrome88,os/android,os/android11,fwv/Bi9jTw.dcqd27,jslib/jquery,htmlplus;am=NL3kFrTJel8ykocDtMl6Xw;ca=script;ts=1650067242;ct=1650067312;th=white;r=0.8826331791215773';
